534247e8e4ea4b81a70d206e32870deff6044497
alex289 <mccr4fthd@gmail.com>
2026-04-09 17:58:05 +0200

    feat: Add initial documentation structure

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,10 +15,91 @@ export const Route = createRootRoute({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "Fumadocs on TanStack Start",
+				name: "description",
+				content: "GitOps for Docker",
+			},
+			{
+				title: "OrcaCD Documentation",
 			},
 		],
-		links: [{ rel: "stylesheet", href: appCss }],
+
+		links: [
+			{ rel: "stylesheet", href: appCss },
+			{
+				rel: "icon",
+				href: "/assets/favicon.ico",
+			},
+			{
+				rel: "apple-touch-icon",
+				href: "/assets/apple-touch-icon.png",
+			},
+			{
+				rel: "icon",
+				type: "image/svg+xml",
+				sizes: "any",
+				href: "/assets/logo-dark.svg",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/assets/logo-dark-32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "64x64",
+				href: "/assets/logo-dark-64.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "96x96",
+				href: "/assets/logo-dark-96.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "128x128",
+				href: "/assets/logo-dark-128.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "144x144",
+				href: "/assets/logo-dark-144.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "180x180",
+				href: "/assets/logo-dark-180.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				href: "/assets/logo-dark-192.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "256x256",
+				href: "/assets/logo-dark-256.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "512x512",
+				href: "/assets/logo-dark-512.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "1024x1024",
+				href: "/assets/logo-dark-1024.png",
+			},
+		],
 	}),
 	component: RootComponent,
 });
